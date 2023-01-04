6a10237f93ba51aee374016acc7042dc1a5eed92
Lukas Altmann <lukasaltmann10@gmail.com>
2023-01-04 10:57:40 +0100

    Initial Commit

---
+++ 3.3 Test/__3_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  __3_TestApp.swift
+//  3.3 Test
+//
+//  Created by Lukas Altmann on 04.01.23.
+//
+
+import SwiftUI
+
+@main
+struct __3_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
